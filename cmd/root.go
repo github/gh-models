6f77e9ce823a1bf3e693eeb1513dd6681d47c9a8
Deborah D <deborah.gertrude.digges@gmail.com>
2024-10-09 19:49:06 +0000

    Add usage template for multi word command

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"github.com/github/gh-models/cmd/list"
 	"github.com/github/gh-models/cmd/run"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 func NewRootCommand() *cobra.Command {
@@ -15,5 +16,10 @@ func NewRootCommand() *cobra.Command {
 	cmd.AddCommand(list.NewListCommand())
 	cmd.AddCommand(run.NewRunCommand())
 
+	// Cobra doesn't have a way to specify a two word command (ie. "gh models"), so set a custom usage template
+	// with `gh`` in it. Cobra will use this template for the root and all child commands.
+	cmd.SetUsageTemplate(strings.NewReplacer(
+		"{{.UseLine}}", "gh {{.UseLine}}",
+		"{{.CommandPath}}", "gh {{.CommandPath}}").Replace(cmd.UsageTemplate()))
 	return cmd
 }
