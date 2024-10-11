09cd4aeb406aa57325bec99df8f4e56debc9b1cd
Sarah Vessels <cheshire137@gmail.com>
2024-10-11 15:51:50 -0500

    List param names
    https: //github.com/github/gh-models/pull/15#discussion_r1797368843
    Co-Authored-By: Christopher Schleiden <cschleiden@live.de>

--- internal/azuremodels/client.go
+++ internal/azuremodels/client.go
@@ -7,7 +7,7 @@ type Client interface {
 	// GetChatCompletionStream returns a stream of chat completions using the given options.
 	GetChatCompletionStream(context.Context, ChatCompletionOptions) (*ChatCompletionResponse, error)
 	// GetModelDetails returns the details of the specified model in a particular registry.
-	GetModelDetails(context.Context, string, string, string) (*ModelDetails, error)
+	GetModelDetails(ctx context.Context, registry, modelName, version string) (*ModelDetails, error)
 	// ListModels returns a list of available models.
 	ListModels(context.Context) ([]*ModelSummary, error)
 }
