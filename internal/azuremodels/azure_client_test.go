2574762a0662f169abc704d4a625229d614c6680
Sarah Vessels <cheshire137@gmail.com>
2024-10-14 17:10:56 -0500

    Start on GetModelDetails test

---
+++ internal/azuremodels/azure_client_test.go
@@ -0,0 +1,24 @@
+package azuremodels
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAzureClient(t *testing.T) {
+	ctx := context.Background()
+	t.Run("GetModelDetails", func(t *testing.T) {
+		token := "fake-token-123abc"
+		client := NewAzureClient(token)
+		registry := "foo"
+		modelName := "bar"
+		version := "baz"
+
+		details, err := client.GetModelDetails(ctx, registry, modelName, version)
+
+		require.NoError(t, err)
+		require.NotNil(t, details)
+	})
+}
