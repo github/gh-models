cb58b6d40a59d2447827f5246a8651aa940ec464
Marais Rossouw <me@marais.co>
2024-10-14 12:06:39 +1000

    chore: move the legal notice to a constants file

---
+++ internal/azuremodels/constants.go
@@ -0,0 +1,3 @@
+package azuremodels
+
+const NOTICE = "ℹ︎ Azure hosted. AI powered, can make mistakes. Not intended for production/sensitive data.\nFor more information, see https://ai.azure.com/github/model/docs"
