411b4511cc63e8047da441cad2951e1e1501398e
Marais Rossouw <me@marais.co>
2024-10-14 12:30:17 +1000

    chore: use gh-cli's term module, instead of relying on cobra

--- internal/azuremodels/legal.go
+++ internal/azuremodels/legal.go
@@ -1,12 +1,17 @@
 package azuremodels
 
 import (
+	"github.com/cli/go-gh/v2/pkg/term"
+	"github.com/github/gh-models/pkg/util"
 	"github.com/mgutz/ansi"
-	"github.com/spf13/cobra"
 )
 
 const notice = "ℹ︎ Azure hosted. AI powered, can make mistakes. Not intended for production/sensitive data.\nFor more information, see https://ai.azure.com/github/model/docs"
 
-func LegalNotice(cmd *cobra.Command) {
-	cmd.PrintErrln(ansi.Color(notice, "yellow"))
+func LegalNotice() {
+	msg := notice
+	if !term.IsColorDisabled() || term.IsColorForced() {
+		msg = ansi.Color(msg, "yellow")
+	}
+	util.WriteToOut(term.FromEnv().ErrOut(), msg+"\n")
 }
