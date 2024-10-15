950bf3a5f2d5435e16fdc2dd784b8863588b1f17
Sarah Vessels <cheshire137@gmail.com>
2024-10-15 11:49:15 -0500

    Add ListModels happy-path test

--- internal/azuremodels/azure_client_test.go
+++ internal/azuremodels/azure_client_test.go
@@ -13,6 +13,65 @@ import (
 func TestAzureClient(t *testing.T) {
 	ctx := context.Background()
 
+	t.Run("ListModels happy path", func(t *testing.T) {
+		summary1 := modelCatalogSearchSummary{
+			AssetID:        "test-id-1",
+			Name:           "test-model-1",
+			DisplayName:    "I Can't Believe It's Not a Real Model",
+			InferenceTasks: []string{"this model has an inference task but the other model will not"},
+			Publisher:      "OpenAI",
+			Summary:        "This is a test model",
+			Version:        "1.0",
+			RegistryName:   "azure-openai",
+		}
+		summary2 := modelCatalogSearchSummary{
+			AssetID:      "test-id-2",
+			Name:         "test-model-2",
+			DisplayName:  "Down the Rabbit-Hole",
+			Publisher:    "Project Gutenberg",
+			Summary:      "The first chapter of Alice's Adventures in Wonderland by Lewis Carroll.",
+			Version:      "THE MILLENNIUM FULCRUM EDITION 3.0",
+			RegistryName: "proj-gutenberg-website",
+		}
+		searchResponse := &modelCatalogSearchResponse{
+			Summaries: []modelCatalogSearchSummary{summary1, summary2},
+		}
+		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Equal(t, "application/json", r.Header.Get("Content-Type"))
+			require.Equal(t, "/", r.URL.Path)
+
+			w.WriteHeader(http.StatusOK)
+			err := json.NewEncoder(w).Encode(searchResponse)
+			require.NoError(t, err)
+		}))
+		defer testServer.Close()
+		cfg := &AzureClientConfig{ModelsURL: testServer.URL}
+		httpClient := testServer.Client()
+		client := NewAzureClient(httpClient, "fake-token-123abc", cfg)
+
+		models, err := client.ListModels(ctx)
+
+		require.NoError(t, err)
+		require.NotNil(t, models)
+		require.Equal(t, 2, len(models))
+		require.Equal(t, summary1.AssetID, models[0].ID)
+		require.Equal(t, summary2.AssetID, models[1].ID)
+		require.Equal(t, summary1.Name, models[0].Name)
+		require.Equal(t, summary2.Name, models[1].Name)
+		require.Equal(t, summary1.DisplayName, models[0].FriendlyName)
+		require.Equal(t, summary2.DisplayName, models[1].FriendlyName)
+		require.Equal(t, summary1.InferenceTasks[0], models[0].Task)
+		require.Empty(t, models[1].Task)
+		require.Equal(t, summary1.Publisher, models[0].Publisher)
+		require.Equal(t, summary2.Publisher, models[1].Publisher)
+		require.Equal(t, summary1.Summary, models[0].Summary)
+		require.Equal(t, summary2.Summary, models[1].Summary)
+		require.Equal(t, summary1.Version, models[0].Version)
+		require.Equal(t, summary2.Version, models[1].Version)
+		require.Equal(t, summary1.RegistryName, models[0].RegistryName)
+		require.Equal(t, summary2.RegistryName, models[1].RegistryName)
+	})
+
 	t.Run("GetModelDetails happy path", func(t *testing.T) {
 		registry := "foo"
 		modelName := "bar"
