9707d1c2e30a145c8bfe3cf5bfc4ca36fe8a8362
Sarah Vessels <cheshire137@gmail.com>
2024-10-15 16:55:28 -0500

    Pull out model_details file

--- internal/azuremodels/types.go
+++ internal/azuremodels/types.go
@@ -2,7 +2,6 @@ package azuremodels
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"github.com/github/gh-models/internal/sse"
 )
@@ -117,25 +116,4 @@ type modelCatalogDetailsResponse struct {
 	CreatedTime        string                        `json:"createdTime"`
 	PlaygroundLimits   *modelCatalogPlaygroundLimits `json:"playgroundLimits"`
 	ModelLimits        *modelCatalogLimits           `json:"modelLimits"`
-}
-
-// ModelDetails includes detailed information about a model.
-type ModelDetails struct {
-	Description               string   `json:"description"`
-	Evaluation                string   `json:"evaluation"`
-	License                   string   `json:"license"`
-	LicenseDescription        string   `json:"license_description"`
-	Notes                     string   `json:"notes"`
-	Tags                      []string `json:"tags"`
-	SupportedInputModalities  []string `json:"supported_input_modalities"`
-	SupportedOutputModalities []string `json:"supported_output_modalities"`
-	SupportedLanguages        []string `json:"supported_languages"`
-	MaxOutputTokens           int      `json:"max_output_tokens"`
-	MaxInputTokens            int      `json:"max_input_tokens"`
-	RateLimitTier             string   `json:"rateLimitTier"`
-}
-
-// ContextLimits returns a summary of the context limits for the model.
-func (m *ModelDetails) ContextLimits() string {
-	return fmt.Sprintf("up to %d input tokens and %d output tokens", m.MaxInputTokens, m.MaxOutputTokens)
 }
